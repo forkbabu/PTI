a8d62bfaff27535150c3ff6948ae58cdf0f569b7
___ <sayantandas30011998@gmail.com>
2022-01-18 13:50:15 -0500

    Update paths_config.py

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -29,3 +29,5 @@ interfacegan_age = 'editings/interfacegan_directions/age.pt'
 interfacegan_smile = 'editings/interfacegan_directions/smile.pt'
 interfacegan_rotation = 'editings/interfacegan_directions/rotation.pt'
 ffhq_pca = 'editings/ganspace_pca/ffhq_pca.pt'
+
+seg_path = "/content/faceparse.pth"
